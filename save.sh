4b19f43b542a5a9043e083d66e1b55ff24c8b932
Cédric Kheirallah <cedric.kheirallah@student.uclouvain.be>
2024-12-08 15:29:32 +0100

    29-11-first topology (tp)

---
+++ save.sh
@@ -0,0 +1,15 @@
+
+SAVE_PATH="clab-igp"
+
+docker exec -it clab-igp-s1 vtysh -c "write memory"
+docker exec -it clab-igp-s1 cat "/etc/frr/frr.conf" > ${SAVE_PATH}/s1/frr.conf
+docker exec -it clab-igp-s2 vtysh -c "write memory"
+docker exec -it clab-igp-s2 cat "/etc/frr/frr.conf" > ${SAVE_PATH}/s2/frr.conf
+docker exec -it clab-igp-s3 vtysh -c "write memory"
+docker exec -it clab-igp-s3 cat "/etc/frr/frr.conf" > ${SAVE_PATH}/s3/frr.conf
+docker exec -it clab-igp-s4 vtysh -c "write memory"
+docker exec -it clab-igp-s4 cat "/etc/frr/frr.conf" > ${SAVE_PATH}/s4/frr.conf
+docker exec -it clab-igp-s5 vtysh -c "write memory"
+docker exec -it clab-igp-s5 cat "/etc/frr/frr.conf" > ${SAVE_PATH}/s5/frr.conf
+docker exec -it clab-igp-s6 vtysh -c "write memory"
+docker exec -it clab-igp-s6 cat "/etc/frr/frr.conf" > ${SAVE_PATH}/s6/frr.conf
